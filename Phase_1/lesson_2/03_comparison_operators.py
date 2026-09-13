c6a6554b9ea72de34c2497efe433b117205079bb
bhutuklearning <amritanshugoutam.dev@gmail.com>
2026-09-13 18:01:40 +0530

    Comparison and Logical Operators

---
+++ Phase_1/lesson_2/03_comparison_operators.py
@@ -0,0 +1,3 @@
+
+age = 22
+print(age >= 18)
---
+++ Phase_1/lesson_2/04_logical_operator.py
@@ -0,0 +1,16 @@
+"""Logical Operators
+Logical operators are used to combine conditional statements."""
+# and, or, not
+age=22
+has_started=True
+print(age>=18 and has_started)
+
+
+is_admin = False
+is_owner = True
+
+print(is_admin or is_owner)
+
+
+is_authenticated = True
+print(not is_authenticated)
