7dcadf82aee028255af64824550847f0de2ca4c2
bhutuklearning <amritanshugoutam.dev@gmail.com>
2026-09-13 18:05:59 +0530

    Conditionals

---
+++ Phase_1/lesson_2/05_conditionals.py
@@ -0,0 +1,23 @@
+"""Conditionals"""
+age = 22
+
+if age>=18:
+    print("You are eligible to vote.")
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+
+#elif
+score = 85
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("D")
+
